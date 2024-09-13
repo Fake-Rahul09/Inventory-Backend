670d007dc6717cfe08208defba1e7186b78e17aa
Rahul009 <137173027+Fake-Rahul09@users.noreply.github.com>
2024-09-13 22:19:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://marvelous-puppy-befb49.netlify.app/"],
+    origin: ["https://inventory-frontend-iota.vercel.app/"],
     credentials: true,
   })
 );
