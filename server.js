51143e67d48d2d2b4ef2b8bba1e831cd32a7ca97
Rahul009 <137173027+Fake-Rahul09@users.noreply.github.com>
2024-09-13 20:41:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://66e441951161e2008b751793--clever-licorice-49669d.netlify.app/"],
+    origin: ["http://localhost:3000", "https://moonlit-truffle-f3b2a2.netlify.app/"],
     credentials: true,
   })
 );
